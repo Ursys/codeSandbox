aecd5e88441f4c0f20b0d97cf07226bc992e2fe4
Ursys <jebs.junkmail@gmail.com>
2015-03-13 15:00:53 -0400

    Adding some stuff
    Adding Arduino folder for sketches
    adding some stuff for creepage and clearance app

--- Chrome Apps/CreepageClearance/tester.js
+++ Chrome Apps/CreepageClearance/tester.js
@@ -8,14 +8,22 @@ function createBarrier () {
 }
 
 function processBarrier() {
-  var input = document.getElementById("BarrierName").value;
-  if (isNaN(input)) {
-    document.getElementById("result").innerHTML = input + " is not a number. Please Enter A Number";
-  } else {
-    if (input % 2 === 0) {
-      document.getElementById("result").innerHTML = input + " is an EVEN number";
-    } else {
-      document.getElementById("result").innerHTML = input + " is an ODD number";
-    }
-  }
+  var tableID = document.getElementById("barrierMatrix");
+  addTableRow(tableID);
+}
+
+function addTableRow(tableID) {
+  //Get a reference to the table
+  var tableRef = document.getElementById(tableID);
+  
+  // Insert a row in the table at row index 0
+  var newRow = tableRef.insertRow(0);
+  
+  // Insert a cell in the row at index 0
+  var newCell = newRow.insertCell(0);
+  
+  // Append a text node to the cell
+  
+  var newText = document.createTextNode('New top row');
+  newCell.appendChild(newText);
 }
